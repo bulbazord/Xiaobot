fc681352be0ad4ab6a3463e802c3b29426669e84
Alexander Langford <nirvashtzero@gmail.com>
2015-06-17 20:13:43 -0400

    Decided to javadoc everything. Also, replies to PINGs with PONGs.

--- src/main/java/me/bulbazord/xiaobot/Xiaobot.java
+++ src/main/java/me/bulbazord/xiaobot/Xiaobot.java
@@ -2,6 +2,8 @@ package me.bulbazord.xiaobot;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Xiaobot{
 
@@ -12,18 +14,39 @@ public class Xiaobot{
 
     private boolean running;
 
+    /**
+     * Default constructor, sets up default configurations.
+     */
     public Xiaobot() {
         this.running = true;
     }
 
+    /**
+     * Returns the bot's socket.
+     * 
+     * @return The bot's socket.
+     */
     public Socket getSocket() {
         return this.socket;
     }
     
+    /**
+     * Method to see if the bot is currently running.
+     *
+     * @return  A boolean representing whether or not the bot is 
+     *          currently running.
+     */
     public boolean isRunning() {
         return this.running;
     }
 
+    /**
+     * Attempt to connect to an IRC network.
+     * 
+     * @param hostname The hostname of the network you wish to connect to.
+     * @param port The port number
+     * @return True if connection successful, false otherwise.
+     */
     public boolean connect(String hostname, int port) {
         try {
             this.socket = new Socket(hostname, port);
@@ -34,6 +57,7 @@ public class Xiaobot{
             return true;
         } catch (UnknownHostException e) {
             System.err.println("The IP of the host could not be determined.");
+            e.printStackTrace();
         } catch (IOException e) {
             System.err.println("Could not open socket");
             e.printStackTrace();
@@ -41,6 +65,9 @@ public class Xiaobot{
         return false;
     }
 
+    /**
+     * Attempt to close connection.
+     */
     public void close() {
         try {
             socket.close();
@@ -50,6 +77,11 @@ public class Xiaobot{
         }
     }
 
+    /**
+     * Check if there is anything to read in the input stream.
+     *
+     * @return True if there is something to read. False otherwise.
+     */
     public boolean readReady() {
         try {
             return this.buffread.ready();
@@ -59,6 +91,11 @@ public class Xiaobot{
         return false;
     }
 
+    /**
+     * Read a line from the input stream.
+     *
+     * @return The line being read.
+     */
     public String readLine() {
         try {
             return this.buffread.readLine();
@@ -68,6 +105,11 @@ public class Xiaobot{
         return null;
     }
 
+    /**
+     * A method to send a message/line to a server.
+     *
+     * @param line The line to be sent.
+     */
     public void sendLine(String line) {
         try {
             buffwrite.write(line + "\r\n");
@@ -78,19 +120,42 @@ public class Xiaobot{
         }
     }
 
+    /**
+     * Method to parse incoming line.
+     *
+     * @param line The line to parse.
+     */
     public void parseLine(String line) {
+        System.out.println(System.currentTimeMillis() + " - " + line);
         try {
-            //System.out.println(System.currentTimeMillis() + " - " + line);
             String[] messageComponents = line.split("\\s", 3);
-            for (int i = 0; i < messageComponents.length; i++) {
-                System.out.print(messageComponents[i] + " | ");
+            String sender;
+            // Not all commands have a sender -- See PING
+            if (messageComponents[0].startsWith(":")) {
+                sender = messageComponents[0];
+                messageComponents = Arrays.copyOfRange(messageComponents, 1, messageComponents.length);
             }
-            System.out.println();
+            //TODO switch statement, handle commands
+            switch(messageComponents[0]) {
+
+            case "PING":
+                sendLine("PONG " + messageComponents[1]);
+                break;
+
+            default:
+                break;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Main method of xiaobot. 
+     * Handles connecting and parsing lines.
+     *
+     * @param args Command line args fed into program.
+     */
     public static void main(String args[]) {
         if (args.length != 2) {
             System.out.println("Please provide server and port number only");
@@ -100,14 +165,16 @@ public class Xiaobot{
         int port = Integer.parseInt(args[1]);
 
         Xiaobot xiaobot = new Xiaobot();
-        xiaobot.connect(hostname, port);
+        boolean connected = xiaobot.connect(hostname, port);
 
-        xiaobot.sendLine("NICK xiaobot");
-        xiaobot.sendLine("USER xiaobot 8 * :xiaobot");
-        while (xiaobot.isRunning()) {
-            while (xiaobot.readReady()) {
-                String incomingLine = xiaobot.readLine();
-                xiaobot.parseLine(incomingLine);
+        if (connected) {
+            xiaobot.sendLine("NICK xiaobot");
+            xiaobot.sendLine("USER xiaobot 8 * :xiaobot");
+            while (xiaobot.isRunning()) {
+                while (xiaobot.readReady()) {
+                    String incomingLine = xiaobot.readLine();
+                    xiaobot.parseLine(incomingLine);
+                }
             }
         }
     }
